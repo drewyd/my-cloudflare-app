c98deaf404f35f5d01f0f0d21513f1b2e119b51a
drewyd <135306998+drewyd@users.noreply.github.com>
2025-04-02 10:51:44 +1100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+  env: {
+    SB_COMPANY_LOGIN: process.env.SB_COMPANY_LOGIN,
+    SB_API_TOKEN: process.env.SB_API_TOKEN,
+  },
+};
 
-export default nextConfig;
+module.exports = nextConfig;
